d214763ea432a047baaa0c40dfbd91a63f7491a4
Nikolai Pesudovs <npesudovs@outlook.com>
2025-10-21 13:09:39 +1030

    cleaned samaras commits

--- include/Game.h
+++ include/Game.h
@@ -1,33 +1,73 @@
 //
 // Created by Nikolai Pesudovs on 14/10/2025.
 //
-
 #ifndef GAME_H
 #define GAME_H
+
 #include <map>
 #include <string>
-
-#include "Level.h"
 #include "Object.h"
+#include "Level.h"
 
-class Game {
- private:
-  static std::map<std::string, Level*> scenes;
-  static Level* current_scene;
+#include <SFML/Graphics.hpp>
+#include "PlayerThief.h"
+#include <iostream>
 
-  // Private constructor to prevent instantiation
-  Game() = delete;
-  Game(const Game&) = delete;
-  Game& operator=(const Game&) = delete;
-
- public:
-  static void initialize(std::map<std::string, Level*> scenes);
-  static void cleanup();
-  static void init();
-  static void update(float deltaTime);
-  static void render(float deltaTime);
-  static void close();
-  static void change_current_scene(Level* scene);
+enum class GameState
+{
+    MAIN_MENU,
+    LEVEL,
+    VICTORY
 };
 
-#endif  // GAME_H
+class Game
+{
+private:
+    sf::RenderWindow &window;
+    GameState state;
+
+    // erm sprite stuff
+    // tests if they all load in and pukks them from sprites folder
+    sf::Texture playerTex, buttonTex, heartTex, diamondTex;
+    sf::Sprite player, button, heart, diamond;
+
+    // life system (ie 3 is max health)
+    sf::Sprite hearts[3];
+    sf::Sprite menuButton;
+
+    PlayerThief playerObj;
+    sf::Clock clock;
+
+    // gang we cant do arial I dont have the file :(
+    sf::Font font;
+    sf::Text menuText;
+
+    // tracks levels beaten in a session :3
+    int levelsBeaten;
+    sf::Sprite diamondIcon;
+    sf::Text diamondCount;
+
+    // sussy baka system
+    int suspicion;
+    int heartsRemaining;
+    float suspicionTimer;
+
+    // sound bar (gang I made it scrollable)
+    float soundLevel;
+    sf::RectangleShape soundBarBG, soundBar;
+
+    // levelzzzzz
+    sf::Sprite levelButtons[3];
+    sf::Text levelText[3];
+
+public:
+    Game(sf::RenderWindow &win);
+    void run();
+
+private:
+    void processEvents();
+    void update();
+    void render();
+};
+
+#endif // GAME_H
--- include/Object.h
+++ include/Object.h
@@ -1,26 +1,29 @@
 #ifndef OBJECT_H
 #define OBJECT_H
-#include <string>
 
-class Object {
- protected:
-  std::string sprite;
-  bool hasGravity;
-  float posX;
-  float posY;
-  float sizeX;
-  float sizeY;
+class Object
+{
+protected:
+    // ImageFilepath sprite;
+    bool hasGravity;
+    float posX;
+    float posY;
+    float sizeX;
+    float sizeY;
 
- public:
-  Object();
-  Object(bool gravity, float X, float Y, float sizeX, float sizeY);
-  void setPosX(float newX);
-  void setPosY(float newY);
-  void moveX(float X);
-  void moveY(float Y);
-  float getX();
-  float getY();
-  void handleGravity(float gravity);
+public:
+    Object(bool, float, float, float, float);
+    void setPosX(float);
+    void setPosY(float);
+    float getSizeX();
+    float getSizeY();
+
+    Object()
+    {
+        hasGravity = false;
+        posX = posY = 0;
+        sizeX = sizeY = 0;
+    }
 };
 
 #endif
