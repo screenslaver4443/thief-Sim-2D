31dc14d346d11cbf26a15790f54ab72ceead103e
Samara <Samara.Costello@gmail.com>
2025-10-21 15:00:46 +1030

    balls

--- include/Level.h
+++ include/Level.h
@@ -1,6 +1,11 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Object.h"
+
+// Base Level classss which we gonna use in LevelOne, LevelTwo, and LevelThree
 class Level
 {
 private:
@@ -8,8 +13,20 @@ private:
     int height;
 
 public:
+    Level(int width, int height, float gravity, std::vector<Object> contents);
+    Level() = default;
+
     void setWidth(int);
     void setHeight(int);
+    int getWidth() const;
+    int getHeight() const;
+
+    sf::Color backgroundColor;
+    sf::Vector2f diamondPosition;
+    float suspicionRate;
+
+    virtual void setup() = 0;
+    virtual ~Level() = default;
 };
 
-#endif
+#endif // LEVEL_H
